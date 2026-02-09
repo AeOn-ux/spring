538170d810fece08f1f7b2eb43732a1ccf0b8568
aeo <hjeei1128@naver.com>
2026-02-09 18:37:01 +0900

    복습

---
+++ s0209_09/src/main/java/com/java/controller/FController.java
@@ -0,0 +1,31 @@
+package com.java.controller;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class FController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/Main")
+	public String Main() {
+		return "Main";
+	}
+}
