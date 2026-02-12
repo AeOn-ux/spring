9c3eaeb51ac59a47f469e856a8ec020ff1532275
aeo <hjeei1128@naver.com>
2026-02-12 10:20:06 +0900

    .

---
+++ s0212_01/src/main/java/com/java/S021205Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S021205Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S021205Application.class, args);
+	}
+
+}
---
+++ s0212_01/src/main/java/com/java/service/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package com.java.service;
+
+import org.springframework.stereotype.Service;
+
+import com.java.dto.MemberDto;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Override
+	public MemberDto selectLogin(MemberDto mdto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
